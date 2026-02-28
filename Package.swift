acd0ff8cb4a5276a12f31529a94a30832b19cffb
zerry <1400852942@qq.com>
2026-02-28 18:01:16 +0800

    Swift 版本兼容 syntax

--- Package.swift
+++ Package.swift
@@ -26,8 +26,8 @@ let package = Package(
         )
     ],
     dependencies: [
-        // Depend on the Swift 5.9+ SwiftSyntax for macro support
-        .package(url: "https://github.com/swiftlang/swift-syntax", from: "509.0.0")
+        // Depend on the latest Swift 5.9 SwiftSyntax
+        .package(url: "https://github.com/swiftlang/swift-syntax", "600.0.0"..<"700.0.0")
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
